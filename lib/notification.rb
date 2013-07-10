4d95b25fa5a737f1b5e6f9b88ff51f025988b030
Jeff Douglas <jeff@jeffdouglas.com>
2013-07-10 16:00:03 -0400

    fixed typo

--- lib/notification.rb
+++ lib/notification.rb
@@ -37,8 +37,7 @@ module Notification
 
       # get the comment
       comment = query_salesforce("select Id, Name, Member__c, Member__r.name, Member__r.Profile_Pic__c,
-        Comment__c, Reply_To__c from Challenge_Comment__c where id = '"
-        +mail.challenge_comment+"' limit 1").first		
+        Comment__c, Reply_To__c from Challenge_Comment__c where id = '"+mail.challenge_comment+"' limit 1").first		
 
       # get all of the non-participant members
       recipients = all_challenge_recipients(mail.challenge)
